ba3c355a69243f7dc9efbbe96f69d60a4e2f9870
Conjr123 <CRichardson@spartaglobal.com>
2021-08-03 15:25:25 +0100

    Main and adjusted functions

--- center_name_gen.py
+++ center_name_gen.py
@@ -1,4 +1,10 @@
 def name_generator(list_of_centers, num_centers):
+    """
+    Appends empty centers to the original list
+    :param list_of_centers: dictionary containing centers and trainees
+    :param num_centers: number of centers to be created
+    :return:
+    """
     for i in range(num_centers):
         if list_of_centers == {}:
             list_of_centers.update({"Center 1": 0})
--- num_full_centers.py
+++ num_full_centers.py
@@ -1,7 +1,5 @@
 def num_full(centers):
-    """
     Takes the dictionary of centers and number of trainees and returns the number of full centers
-    """
     full = 0
     num = centers.values()
     # Creates a list of trainees per centers
