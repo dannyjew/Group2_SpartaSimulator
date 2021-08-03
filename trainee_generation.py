9f52a331ce0c4b6206d5bb7ece535ce8fb2994dd
Vrinda Gera <88028643+CodingUnicorn15@users.noreply.github.com>
2021-08-03 15:12:33 +0100

    Update trainee_generation.py

--- trainee_generation.py
+++ trainee_generation.py
@@ -1,8 +1,6 @@
-import random
-
-number_trainees = [] # to test append if needed
+# here we are going to create a random number of students between 20 - 30 for each month
 def trainee_gen():
-    number = random.randint(20, 30)
-    return number
+    import random # we need function in this library
+    number = random.randint(20, 30) # this is inclusive for 20 and 30
+    return number # this will give us this random number to use for further work
 
-print(trainee_gen()) #to test number
